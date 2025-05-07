947bc20b1bad70dd179cc0a88cb0dba532d18412
sa-bri-na <huber_sabrina@me.com>
2025-05-07 22:17:43 +0200

    Build for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://ingeniumapps.github.io/karriere-kolleg/',
+    outDir: 'docs',
+});
