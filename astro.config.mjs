4c013ae22acaa66ff38ca0475482e05aa46f6de8
sa-bri-na <huber_sabrina@me.com>
2025-05-12 14:47:15 +0200

    add base link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://ingeniumapps.github.io/karriere-kolleg/', // ✔︎ bleibt
-    //base: '/karriere-kolleg/',                               // ← NEU!
+    base: '/karriere-kolleg/',                               // ← NEU!
     outDir: 'docs',
 });
